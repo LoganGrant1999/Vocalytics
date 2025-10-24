e2ac85e9bcb5ffc0ae2843f4d1148b0f5004d51c
Logan Gibbons <lgg25@byu.edu>
2025-10-24 10:15:10 -0600

    Fix Vercel handler to use native handler instead of AWS Lambda adapter

--- packages/server/api/index.ts
+++ packages/server/api/index.ts
@@ -1,12 +1,1 @@
-import awsLambdaFastify from '@fastify/aws-lambda';
-import { createHttpServer } from '../src/http/index.js';
-
-let handler: any;
-
-export default async function (req: any, res: any) {
-  if (!handler) {
-    const app = await createHttpServer();
-    handler = awsLambdaFastify(app);
-  }
-  return handler(req, res);
-}
+export { default } from '../src/http/index.js';
